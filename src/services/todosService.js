ee4e9daefef0ac0b9887e387b0f4e449475a8c3c
Medusaa36 <rajahasbullah06@gmail.com>
2026-04-30 15:59:48 +0700

    update todos servise

--- src/services/todosService.js
+++ src/services/todosService.js
@@ -1,26 +1,57 @@
 import todos from "../models/todos.js";
 
-const getAllTodos = async () => {
-  return await todos.find().sort({ createdAt: 1 });
+const getAllTodos = async (userId) => {
+  return await todos.find({ user: userId }).sort({ createdAt: 1 });
 };
 
-const createTodo = async (data) => {
-  return await todos.create(data);
+const createTodo = async (data, userId) => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const dueDate = new Date(data.dueDate);
+
+  if (dueDate < today) {
+    throw new Error("Tanggal deadline tidak boleh sebelum hari ini");
+  }
+
+  return await todos.create({
+    ...data,
+    user: userId,
+  });
 };
 
-const deleteTodo = async (id) => {
-  return await todos.findByIdAndDelete(id);
+const deleteTodo = async (id, userId) => {
+  return await todos.findOneAndDelete({
+    _id: id,
+    user: userId,
+  });
 };
 
-const updateTodo = async (id, data) => {
-  return await todos.findByIdAndUpdate(id, data, { new: true });
+const updateTodo = async (id, data, userId) => {
+  return await todos.findOneAndUpdate({ _id: id, user: userId }, data, {
+    new: true,
+  });
 };
 
-const toggleComplete = async (id) => {
-  const todo = await todos.findById(id);
+const toggleComplete = async (id, userId) => {
+  const todo = await todos.findOne({
+    _id: id,
+    user: userId,
+  });
 
   if (!todo) {
-    throw new Error("Data tidak ditemukan");
+    throw new Error("Data tidak ditemukan / bukan milik user");
+  }
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const dueDate = new Date(todo.dueDate);
+
+  if (!todo.completed && dueDate < today) {
+    throw new Error(
+      "Tugas sudah melewati deadline dan tidak dapat diselesaikan",
+    );
   }
 
   todo.completed = !todo.completed;

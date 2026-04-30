8d28be507ee6333c74bcbba60187d7942c5d6440
Medusaa36 <rajahasbullah06@gmail.com>
2026-04-30 15:58:35 +0700

    update todos controller

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -7,37 +7,33 @@ import {
 } from "../services/todosService.js";
 
 const getAll = async (req, res) => {
-  const allTodos = await getAllTodos();
+  const allTodos = await getAllTodos(req.user.id);
 
   res.render("todos/index", {
     layout: "layouts/main",
     data: allTodos,
-    success: req.flash("success"),
-    error: req.flash("error"),
   });
 };
 
 const postTodos = async (req, res) => {
   try {
-    await createTodo(req.body);
+    await createTodo(req.body, req.user.id);
 
     return res.status(201).json({
       success: true,
       message: "Tugas berhasil ditambahkan",
     });
   } catch (error) {
-    console.log(error);
-
-    return res.status(500).json({
+    return res.status(400).json({
       success: false,
-      message: "Gagal menambahkan tugas",
+      message: error.message,
     });
   }
 };
 
 const deleteTodos = async (req, res) => {
   try {
-    await deleteTodo(req.params.id);
+    await deleteTodo(req.params.id, req.user.id);
 
     req.flash("success", "Tugas Berhasil Dihapus");
     res.redirect("/todos");
@@ -49,32 +45,34 @@ const deleteTodos = async (req, res) => {
   }
 };
 
-const updateTodos = async (req, res) => {
-  try {
-    await updateTodo(req.params.id, {
-      title: req.body.title,
-      description: req.body.description,
-    });
+const updateTodos = async (id, data, userId) => {
+  if (data.dueDate) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-    req.flash("success", "Tugas Berhasil Diedit");
-    res.redirect("/todos");
-  } catch (error) {
-    console.log(error);
+    const dueDate = new Date(data.dueDate);
 
-    req.flash("error", "Gagal Edit Tugas");
-    res.redirect("/todos");
+    if (dueDate < today) {
+      throw new Error("Tanggal deadline tidak valid");
+    }
   }
+
+  return await todos.findOneAndUpdate({ _id: id, user: userId }, data, {
+    new: true,
+  });
 };
 
 const completedBtn = async (req, res) => {
   try {
-    const todo = await toggleComplete(req.params.id);
+    const todo = await toggleComplete(req.params.id, req.user.id);
 
     res.json({
       message: todo.completed ? "Tugas selesai" : "Tugas dibatalkan",
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(400).json({
+      message: error.message,
+    });
   }
 };
 

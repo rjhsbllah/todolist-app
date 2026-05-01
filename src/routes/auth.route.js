4f55315bc81ec5294a428e59f0482ba7efd4c107
Medusaa36 <rajahasbullah06@gmail.com>
2026-05-01 16:58:55 +0700

    add route profile page

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,16 +4,28 @@ import {
   register,
   login,
   logout,
+  updateUsername,
+  updatePassword,
 } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const authRouter = express.Router();
 
+// register
 authRouter.get("/register", showAuthPage);
 authRouter.post("/register", register);
-
+// login
 authRouter.get("/login", showAuthPage);
 authRouter.post("/login", login);
-
+// logout
 authRouter.get("/logout", logout);
+// profile page
+authRouter.get("/profile", authMiddleware, (req, res) => {
+  res.render("profile/profile");
+});
+// update username
+authRouter.post("/profile/username", authMiddleware, updateUsername);
+// update password
+authRouter.post("/profile/password", authMiddleware, updatePassword);
 
 export default authRouter;

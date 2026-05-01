b318fcb2112854f0325def0691bd903c09428c2f
Medusaa36 <rajahasbullah06@gmail.com>
2026-05-01 16:58:30 +0700

    update middleware auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,11 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
+
+    res.locals.user = decoded;
+
     next();
   } catch (err) {
     return res.redirect("/login");

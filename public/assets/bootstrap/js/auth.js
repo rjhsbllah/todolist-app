d3e2371de8bac330d2257b23a8236e153c9c9327
Medusaa36 <rajahasbullah06@gmail.com>
2026-05-01 16:57:21 +0700

    add toggle pasword

--- public/assets/bootstrap/js/auth.js
+++ public/assets/bootstrap/js/auth.js
@@ -1,10 +1,35 @@
-let container = document.getElementById("container");
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
 
-function toggle() {
-  container.classList.toggle("sign-in");
-  container.classList.toggle("sign-up");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleIcons = document.querySelectorAll(".toggle-password");
 
-setTimeout(() => {
-  container.classList.add("sign-in");
-}, 200);
+  toggleIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      const input = icon.previousElementSibling;
+
+      if (input.type === "password") {
+        input.type = "text";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+      } else {
+        input.type = "password";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+      }
+    });
+  });
+});

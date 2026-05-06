66296cbbdd8b6281daa934125a3dc4cf7ad511e4
Medusaa36 <rajahasbullah06@gmail.com>
2026-05-06 07:29:13 +0700

    update countdown deadline script

--- public/assets/bootstrap/js/countdownDeadline.js
+++ public/assets/bootstrap/js/countdownDeadline.js
@@ -1,22 +1,16 @@
-function getWIBTime() {
-  const now = new Date();
-
-  // offset dalam menit
-  const localOffset = now.getTimezoneOffset();
-  const wibOffset = 7 * 60;
-
-  return new Date(now.getTime() + (wibOffset + localOffset) * 60000);
-}
-
 function updateCountdown() {
   const elements = document.querySelectorAll(".countdown");
 
   elements.forEach((el) => {
-    // pastikan deadline dianggap WIB
-    const deadline = new Date(el.dataset.deadline + " GMT+0700");
-    const now = getWIBTime();
+    const deadline = new Date(el.dataset.deadline).getTime();
+    const now = Date.now();
 
     const diff = deadline - now;
+
+    if (isNaN(diff)) {
+      el.innerHTML = "❌ Format tanggal error";
+      return;
+    }
 
     if (diff <= 0) {
       el.innerHTML = "❌ Deadline terlewati";
